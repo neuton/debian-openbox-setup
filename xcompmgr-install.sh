e28080cb97c54c3ca62d6d19b38a5b49e9cdf8c1
Oleksii Gryniuk <mr.neuton@gmail.com>
2015-01-06 16:38:34 +0200

    xcompmgr-install.sh fix (?)

--- xcompmgr-install.sh
+++ xcompmgr-install.sh
@@ -2,6 +2,7 @@
 echo &&
 echo =============================== setup xcompmgr ================================= &&
 sudo apt-get install -y dh-autoreconf xutils-dev git &&
+#sudo apt-get install -y libx11-dev libxcomposite-dev libxdamage-dev libxrender-dev &&
 git clone http://anongit.freedesktop.org/git/xorg/app/xcompmgr.git &&
 cd xcompmgr && sh autogen.sh && make && sudo make install &&
 cd .. && rm -rf xcompmgr/
